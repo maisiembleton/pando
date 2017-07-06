d41b0234988c33e1a22741787f653992dfceb4e6
Maizeee <aztecorn@gmail.com>
2017-07-06 15:58:56 +0800

    F commit

---
+++ login_effect.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    $("#show_login").click(function() {
+        showpopup();
+    });
+
+});
+
+function showpopup() {
+    $("#loginform").fadeIn();
+    $("#loginform").css({ "visibility": "visible", "display": "block" });
+}
+
+function hidepopup() {
+    $("#loginform").fadeOut();
+    $("#loginform").css({ "visibility": "hidden", "display": "none" });
+}
+
+
+function chUserAndPssword() {
+    var dUsername = document.getElementById("login").value;
+    var dPassword = document.getElementById("password").value;
+
+    if (dUsername == "" || dPassword == "") {
+
+        alert("Please In put data")
+        showpopup()
+
+    } else {
+        inPutdataToserver()
+        hidepopup()
+    }
+
+}
+
+
+function inPutdataToserver() {
+    alert("OK")
+}
